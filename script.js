f2041fc3c6dfdf17fd70df0bc20d41bb187d1aa2
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-06-07 20:04:29 -0600

    changing password length and alert

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function generatePassword() {
   var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
   // Loop if answer is outside the parameters 
-  while(confirmLength <= 7 || confirmLength >= 51) {
+  while(confirmLength <= 7 || confirmLength >= 129) {
       alert("Password length must be between 8-128 characters Try again");
       var confirmLength = (prompt("How many characters would you like your password to contain?"));
       } 
